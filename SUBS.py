b78ff2d39cdc785f2a0a43f99d2698a4b5da7428
Hà Trọng Nghĩa <nghia.hust@gmail.com>
2022-06-29 10:46:11 +0700

    Add files via upload

--- SUBS.py
+++ SUBS.py
@@ -1,6 +1,8 @@
 with open('rosalind_subs.txt','r') as file:
     lines = file.readlines()
     t = lines[0]
-    s = line[1]
-    for i in range(len(t) - 1):
-
+    s = lines[1]
+    print(s)
+    for i in range(len(t) - len(s) -1):
+        if t[i:i+len(s)] == s:
+            print(i+1,end=' ')
