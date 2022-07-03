47cbd6f912577e4b10f32a03cd2908fa1d3787c2
Ha Trong Nghia <hatrongnghia@outlook.com>
2022-07-03 22:55:08 +0700

    Update

--- FIBD.py
+++ FIBD.py
@@ -1,4 +1,13 @@
 with open('rosalind_fibd.txt','r') as file:
-    line = file.readline().strip()
-    n = line[0]
-    m = line[1]
+    line = file.readline().split()
+    n = int(line[0])
+    m = int(line[1])
+    def rabbit(i, j):
+        print(i,j)
+        if i == 1 or i == 2:
+            return 1
+        elif i <= j+1:
+            return rabbit(i-1, j) + rabbit(i-2, j)
+        else:
+            return rabbit(i-1, j) + rabbit(i-2, j) - rabbit(i-j-1, j)
+    print(rabbit(n, m))
