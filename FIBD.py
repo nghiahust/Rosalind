a2e08dcbddcb211a56e52dcd340078cc8028ab83
Ha Trong Nghia <hatrongnghia@outlook.com>
2022-07-05 05:48:54 +0700

    Update

--- FIBD.py
+++ FIBD.py
@@ -2,12 +2,13 @@ with open('rosalind_fibd.txt','r') as file:
     line = file.readline().split()
     n = int(line[0])
     m = int(line[1])
-    def rabbit(i, j):
-        print(i,j)
-        if i == 1 or i == 2:
-            return 1
-        elif i <= j+1:
-            return rabbit(i-1, j) + rabbit(i-2, j)
+    rabbits = [1,1]
+    for month in range(3, n+1):
+        if month < m+1:
+            rabbits.append(rabbits[-1] + rabbits[-2])
+        elif month == m+1:
+            rabbits.append(rabbits[-1] + rabbits[-2] - rabbits[0])
         else:
-            return rabbit(i-1, j) + rabbit(i-2, j) - rabbit(i-j-1, j)
-    print(rabbit(n, m))
+            rabbits.append(rabbits[-1] + rabbits[-2] - rabbits[-m-1])
+        print(len(rabbits), month, rabbits[-1])
+    print(rabbits[-1])
