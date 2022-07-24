805195d7afb2449652045f42578da1905bc7cd26
Ha Trong Nghia <hatrongnghia@outlook.com>
2022-07-25 06:33:32 +0700

    Update

--- Bioinformatics Stronghold/CAT.py
+++ Bioinformatics Stronghold/CAT.py
@@ -5,5 +5,15 @@ for line in lines:
     if line[0] != '>':
         sequence += line.strip()
 n = len(sequence)
-for i in range(1, n+1):
-
+catNum = []
+for i in range(1, n//2+1):
+    if i == 1:
+        catNum.append(1)
+    elif i == 2:
+        catNum.append(2)
+    else:
+        result = 0
+        for j in range(1, len(catNum)):
+            print(i,j)
+            result += catNum[j-1] * catNum[-1-j]
+print(catNum)
