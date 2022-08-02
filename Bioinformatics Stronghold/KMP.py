d33791139cbbfce75d561ac8380cd3fc52d51924
Ha Trong Nghia <hatrongnghia@outlook.com>
2022-08-02 07:32:34 +0700

    in the midding of KMP

--- Bioinformatics Stronghold/KMP.py
+++ Bioinformatics Stronghold/KMP.py
@@ -4,10 +4,10 @@ seq = ''.join([line.strip() for line in lines if line[0] != '>'])
 P = []
 n = len(seq)
 for i in range(n):
-    print(i)
     if i == 0:
         P.append(0)
     else:
+        while k > 0 and seq[k] != seq[  ]
         s = 0
         for k in range(1, (i+1)//2):
             if seq[0: k] == seq[i-k+1: i+1]:
