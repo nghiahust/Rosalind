ccea89c379376fcd6103972d2acc286bb070024a
Ha Trong Nghia <hatrongnghia@outlook.com>
2022-08-03 17:07:47 +0700

    update KMP

--- Bioinformatics Stronghold/KMP.py
+++ Bioinformatics Stronghold/KMP.py
@@ -1,14 +1,14 @@
 with open('rosalind_kmp.txt','r') as file:
     lines = file.readlines()
 seq = ''.join([line.strip() for line in lines if line[0] != '>'])
+print(seq)
 n = len(seq)
 P = [0] * n
 k = 0
 for i in range(1, n):
-    for j in range(1, n-i+1):
-        if seq[:i] == seq[j:j+i]:
-            P[j+i-1] = len(seq[:i])
-            k = len(seq[:i])
-    if k < len(seq[:i]):
-        break
+    while k > 0 and seq[k] != seq[i]:
+        k = P[k-1]
+    if seq[k] == seq[i]:
+        k = k + 1
+    P[i] = k
 print(*P, end=' ')
