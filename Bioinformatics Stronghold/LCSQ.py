714ede6459685399cec9f3bff434bee8439420f0
Ha Trong Nghia <hatrongnghia@outlook.com>
2022-08-08 17:49:55 +0700

    Update LCSQ

--- Bioinformatics Stronghold/LCSQ.py
+++ Bioinformatics Stronghold/LCSQ.py
@@ -11,24 +11,12 @@ seq2 = seqs[1]
 subseq = ''
 n = len(seq1)
 m = len(seq2)
-i, j = 0, 0 # Current possition on seq1 and seq2
-while i < n and i < m:
-    li = i
-    lj = j
-    while li < n-1 or seq1[li] != seq2[j]:
-        li += 1
-        print(i, j, li, lj, seq1[li], seq2[j])
-    while lj < m-1 or seq1[i] != seq2[lj]:
-        lj += 1
-    if li - i < lj - j:
-        subseq += seq1[li]
-        i = li+1
-        j += 1
-    elif li - i > lj - j:
-        subseq += seq2[lj]
-        i += 1
-        j = lj+1
-    else:
-        i = li+1
-        j = lj+1
-print(subseq)
+C = [[0] * m] * n
+def 
+
+#def LCS(seq1, seq2, n, m):
+#    if n == 0 or m == 0:
+#        break
+#    else:
+#        if 
+#print(LCS(seq1, seq2, n, m))
