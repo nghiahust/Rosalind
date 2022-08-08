66b8a5d8778d347f10edfb4ae6b69f146e48b8ec
Ha Trong Nghia <hatrongnghia@outlook.com>
2022-08-09 06:13:54 +0700

    Update

--- Bioinformatics Stronghold/LCSQ.py
+++ Bioinformatics Stronghold/LCSQ.py
@@ -12,12 +12,31 @@ subseq = ''
 n = len(seq1)
 m = len(seq2)
 C = [[0] * m] * n
-def
 
-#def LCS(seq1, seq2, n, m):
-#    if n == 0 or m == 0:
-#        break
-#    else:
-#        if
-#print(LCS(seq1, seq2, n, m))
-i, j = 0, 0 # Current possition on seq1 and seq2
+def LCSL(seq1, seq2):
+    n = len(seq1)
+    m = len(seq2)
+    C = [[0] * m] * n
+    for i in range(n):
+        for j in range(m):
+            if i == 0 or j == 0:
+                if seq1[i] == seq2[j]:
+                    C[i][j] = 1
+            elif seq1[i] == seq2[j]:
+                C[i][j] = C[i-1][j-1] + 1
+            else:
+                C[i][j] = max(C[i][j-1], C[i-1][j])
+        print(*C, sep='\n', end='\n\n')
+    return C
+#print(*LCSL(seq1, seq2), sep= '\n')
+
+def LCS(C, seq1, seq2, n, m):
+    if n == 0 or m == 0:
+        return ''
+    if seq1[n] == seq2[m]:
+        return LCS(C, seq1, seq2, n-1, m-1) + seq1[n]
+    elif C[n][m-1] > C[n-1][m]:
+        return LCS(C, seq1, seq2, n, m-1)
+    else:
+        return LCS(C, seq1, seq2, n-1, m)
+print(LCS(LCSL(seq1, seq2), seq1, seq2, n-1, m-1))
