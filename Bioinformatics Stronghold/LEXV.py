0bf3659381cfc557b23d9dff763791a2ff074a1e
Ha Trong Nghia <hatrongnghia@outlook.com>
2022-08-11 11:13:42 +0700

    Ordering Strings of Varying Length Lexicographically

--- Bioinformatics Stronghold/LEXV.py
+++ Bioinformatics Stronghold/LEXV.py
@@ -8,4 +8,4 @@ for i in range(1, n+1):
     c = list(itertools.product(char_list, repeat=i))
     for x in c:
         lexv.append(''.join(x))
-print(sorted(lexv))
+print(*sorted(lexv, key=lambda x:[char_list.index(a) for a in x]),sep='\n')
