b95d449628e2c873e2ca410eae935ac904a7c431
Ha Trong Nghia <hatrongnghia@outlook.com>
2022-08-11 09:20:49 +0700

    Update

--- Bioinformatics Stronghold/LEXV.py
+++ Bioinformatics Stronghold/LEXV.py
@@ -3,4 +3,5 @@ with open('rosalind_lexv.txt','r') as file:
     lines = file.readlines()
 char_list = lines[0].strip().split()
 n = lines[1]
-print(*itertools.product(char_list, ' ', ' '))
+for char in char_list:
+    fo
