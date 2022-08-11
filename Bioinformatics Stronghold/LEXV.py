38df5c71ee83084f774ac7cc2891e4a78657db36
Ha Trong Nghia <hatrongnghia@outlook.com>
2022-08-11 09:23:57 +0700

    Update

---
+++ Bioinformatics Stronghold/LEXV.py
@@ -0,0 +1,11 @@
+import itertools
+with open('rosalind_lexv.txt','r') as file:
+    lines = file.readlines()
+char_list = lines[0].strip().split()
+n = int(lines[1])
+lexv = []
+for i in range(1, n+1):
+    c = list(itertools.product(char_list, repeat=i))
+    for x in c:
+        lexv.append(''.join(x))
+print(sorted(lexv))
