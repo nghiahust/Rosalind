0a401a67fc338cb542c5997bf64f6c0ddb280564
Ha Trong Nghia <hatrongnghia@outlook.com>
2022-09-01 07:28:05 +0700

    Update

--- Bioinformatics Stronghold/MMCH.py
+++ Bioinformatics Stronghold/MMCH.py
@@ -1,12 +1,9 @@
+from itertools import permutations as permu
 with open('rosalind_mmch.txt','r') as file:
     lines = file.readlines()
 seq = ''.join([line.strip() for line in lines if line[0] != '>'])
 n = len(seq)
-count_A = seq.count('A')
-count_U = seq.count('U')
-count_G = seq.count('G')
-count_C = seq.count('C')
-combiAU = count_A * count_U
-combiGC = count_G * count_C
-result = combiAU * combiGC
-print(result)
+count_nu = {}
+for i in ('A','U','G','C'):
+    count_nu[i] = seq.count(i)
+
