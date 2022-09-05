4bb87cfde3377e0b4d22d7c57e39c6b04801aac4
Ha Trong Nghia <hatrongnghia@outlook.com>
2022-09-05 12:58:34 +0700

    update

--- Bioinformatics Stronghold/MMCH.py
+++ Bioinformatics Stronghold/MMCH.py
@@ -1,4 +1,4 @@
-from itertools import permutations as permu
+from fractions import Fraction
 with open('rosalind_mmch.txt','r') as file:
     lines = file.readlines()
 seq = ''.join([line.strip() for line in lines if line[0] != '>'])
@@ -6,4 +6,5 @@ n = len(seq)
 count_nu = {}
 for i in ('A','U','G','C'):
     count_nu[i] = seq.count(i)
+
 
