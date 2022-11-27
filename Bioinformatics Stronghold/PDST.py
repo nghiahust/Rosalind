adca64436e6600c8851a92c249e710a734013a4d
Ha Trong Nghia <hatrongnghia@outlook.com>
2022-11-27 22:41:03 +0700

    Finish PDST

--- Bioinformatics Stronghold/PDST.py
+++ Bioinformatics Stronghold/PDST.py
@@ -3,4 +3,19 @@ with open('rosalind_pdst.txt', 'r') as file:
 seqs = []
 for line in lines:
     if line[0] == '>':
+        seqs.append('')
+    else:
+        seqs[-1] += line.strip()
+n = len(seqs)
+def pdistance(seq1, seq2):
+    k = 0
+    n = len(seq1)
+    for i in range(n):
+        if seq1[i] != seq2[i]:
+            k += 1
+    return round(k / n, 5)
 
+for i in range(n):
+    for j in range(n):
+        print("{:.5f}".format(pdistance(seqs[i], seqs[j])),end = ' ')
+    print('\n')
