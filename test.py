881724859637231fa6364fe3a4ebe934a11739e5
Ha Trong Nghia <hatrongnghia@outlook.com>
2023-01-02 16:15:29 +0700

    test more

--- test.py
+++ test.py
@@ -2,3 +2,7 @@ import urllib
 code = "Q7Z7W5"
 data = urllib.urlopen("http://www.uniprot.org/uniprot/" + code + ".txt").read()
 print(data)
+
+print('Nghia dep trai')
+for i in range(0,10):
+    print(i)
